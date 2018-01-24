c0aca0ee26d73f29ef05ff84338081ea9c2acc2e
Alessio Pagliari <alessio.pagliari@gmail.com>
2018-01-24 18:40:44 +0100

    implemented parsing of entire string

--- tcpprobe.py
+++ tcpprobe.py
@@ -3,12 +3,17 @@
 class ProbeParser:
     def __init__(self, probe):
        splitted = probe.split()
-       self.ts = float(splitted[0])
-       self.sh = self.getAddress(splitted[1])
-       self.sp = self.getPort(splitted[1])
-       self.sh = self.getAddress(splitted[2])
-       self.dp = self.getPort(splitted[2])
-       self.by = int(splitted[3])
+       self.ts = float(splitted[0])             # Time in seconds
+       self.sh = self.getAddress(splitted[1])   # Source address
+       self.sp = self.getPort(splitted[1])      # Source port
+       self.rh = self.getAddress(splitted[2])   # Destination address
+       self.dp = self.getPort(splitted[2])      # Destination port
+       self.by = int(splitted[3])               # Bytes in packet
+       self.ns = splitted[4]                    # Next send sequence nbr
+       self.us = splitted[5]                    # Unacknowledged sequence nbr
+       self.cw = int(splitted[6])               # Congestion window
+       self.ss = int(splitted[7])               # Slow start threshold
+       self.sw = int(splitted[8])               # Send window
     
     def getAddress(self, app): #app: address plus port
         return app.rsplit(":",1)[0]
