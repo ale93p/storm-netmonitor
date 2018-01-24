33889439c8e3897129f302cbb2ab9bd34b7e2727
Alessio Pagliari <alessio.pagliari@gmail.com>
2018-01-24 17:57:14 +0100

    beta module for parsing tcpprobe strings

---
+++ tcpprobe.py
@@ -0,0 +1,17 @@
+# tcpprobe parser module
+
+class ProbeParser:
+    def __init__(self, probe):
+       splitted = probe.split()
+       self.ts = int(splitted[0])
+       self.sh = self.getAddress(splitted[1])
+       self.sp = self.getPort(splitted[1])
+       self.sh = self.getAddress(splitted[2])
+       self.dp = self.getPort(splitted[2])
+       self.by = int(splitted[3])
+       
+    def.getAddress(app):
+        return app.rsplit(":",1)[0]
+
+    def getPort(app):
+        return int(app.rsplit(":",1)[1])
