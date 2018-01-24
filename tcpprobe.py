e34c50a312e3a8ba0779c3e987ac16474e033705
Alessio Pagliari <alessio.pagliari@gmail.com>
2018-01-24 18:05:01 +0100

    changed timestamp from int to float

--- tcpprobe.py
+++ tcpprobe.py
@@ -3,14 +3,14 @@
 class ProbeParser:
     def __init__(self, probe):
        splitted = probe.split()
-       self.ts = int(splitted[0])
+       self.ts = float(splitted[0])
        self.sh = self.getAddress(splitted[1])
        self.sp = self.getPort(splitted[1])
        self.sh = self.getAddress(splitted[2])
        self.dp = self.getPort(splitted[2])
        self.by = int(splitted[3])
-       
-    def getAddress(app):
+    
+    def getAddress(app): #app: address plus port
         return app.rsplit(":",1)[0]
 
     def getPort(app):
