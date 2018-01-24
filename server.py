e4dbee6dd19fcdc64f6d6898738e5182a6c79b9a
Alessio Pagliari <alessio.pagliari@gmail.com>
2018-01-24 14:44:36 +0100

    networkInser first version

--- server.py
+++ server.py
@@ -14,9 +14,23 @@ def test():
 
 @app.route("/api/v0.1/network/insert", methods=['GET'])
 def networkInsert():
-    var = request.args["var"]
-    print var
-    return "Ok"
+    try:
+        ts = request.args["ts"]
+
+        src_host = request.args["src_host"]
+        src_port = request.args["src_port"]
+
+        dst_host = request.args["dst_host"]
+        dst_port = request.args["dst_port"]
+
+        pkts = request.args["pkts"]
+        bts = request.args["bytes"]
+    
+    except:
+        return "ERROR"
+
+    print ("[DEBUG] Received from client: (",ts," ",src_host,":",src_port,"->",dst_host,":",dst_port," ",pkts,"pkts ",bts,"Bytes)")
+    return True
 
 if __name__ == "__main__":
     app.run()
