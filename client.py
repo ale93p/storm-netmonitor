74b14c298695b7c3d0419fff041831151aa27e21
Alessio Pagliari <alessio.pagliari@gmail.com>
2018-01-24 15:57:18 +0100

    print something for debug

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ from optparse import OptionParser
 
 def networkInsert(ts, sh, sp, dh, dp, pk, by):
     url = "http://127.0.0.1:5000/api/v0.1/insert"
-    var = requests.get(url + "?ts=" + str(ts) + "src_host=" + str(sh) + "&src_port=" + str(sp) + "&dst_host=" + str(dh) + "&dst_port=" + str(dp) + "&pkts=" + str(pk) + "&bytes=" + str(by))
-
+    return requests.get(url + "?ts=" + str(ts) + "src_host=" + str(sh) + "&src_port=" + str(sp) + "&dst_host=" + str(dh) + "&dst_port=" + str(dp) + "&pkts=" + str(pk) + "&bytes=" + str(by))
+    
 if __name__ == "__main__":
     usage = "usage: %prog -s SERVER_ADDR [-p]"
     parser = OptionParser(usage=usage)
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         print("You must specify a server address")
 
     while True:
-        networkInsert(time.time(),"dummy1",1234,"dummy2",4321,15,3287)
+        print(networkInsert(time.time(),"dummy1",1234,"dummy2",4321,15,3287))
         time.sleep(5)
