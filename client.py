b29ff81eba823eb4c7a91402109ff959ead2d080
Alessio Pagliari <alessio.pagliari@gmail.com>
2018-01-24 15:49:21 +0100

    options and dummy test

--- client.py
+++ client.py
@@ -1,10 +1,22 @@
 import requests
 import json
+import time
+from optparse import OptionParser
 
-url = "http://127.0.0.1:5000/api/v0.1/insert"
+def networkInsert(ts, sh, sp, dh, dp, pk, by):
+    url = "http://127.0.0.1:5000/api/v0.1/insert"
+    var = requests.get(url + "?ts=" + str(ts) + "src_host=" + str(sh) + "&src_port=" + str(sp) + "&dst_host=" + str(dh) + "&dst_port=" + str(dp) + "&pkts=" + str(pk) + "&bytes=" + str(by))
 
+if __name__ == "__main__":
+    usage = "usage: %prog -s SERVER_ADDR [-p]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-s", "--server", dest="server_addr", help="specify server address")
+    parser.add_option("-p", "--port", dest="server_port", help="specify server listening port", default="5000")
+    (options, args) = parser.parse_args()
 
+    if not options.server_addr:
+        print("You must specify a server address")
 
-var = requests.get(url + "?src_host=sdn1)
-
-print var.json()
+    while True:
+        networkInsert(time.time(),"dummy1",1234,"dummy2",4321,15,3287)
+        time.sleep(5)
